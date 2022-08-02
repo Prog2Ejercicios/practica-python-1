341728a7d41e492ac0fdcafb3e28afd07c494ec9
LeonelZarini <leonelzarini@hotmail.com>
2022-08-01 22:04:54 -0300

    Grupo nro 1

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+print("Esta es su calculadora")
+print("Ingrese su primer numero:")
+num=int(input())
+print("Ingrese su segundo numero:")
+num2=int(input())
+
+print("La suma de ambos numeros es:"+ str(num + num2))
+print("La diferencia de los numeros es: "+ str(num-num2))
+print("La división de los mismos es :"+ str(num/num2))
+print("Su multiplicación es :"+ str(num*num2))
