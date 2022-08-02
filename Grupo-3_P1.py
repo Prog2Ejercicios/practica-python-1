304463f5cd0679cf029b0789e2be891534e6371c
Mauricio-Roldan <roldanmauriciomax@gmail.com>
2022-08-01 21:49:34 -0300

    Practica 1 Grupo 3

---
+++ Grupo-3_P1.py
@@ -0,0 +1,12 @@
+n1= int(input("ingrese el primer numero: "))
+n2= int(input("ingrese el segundo numero: "))
+
+suma= n1+n2
+resta= n1-n2
+division= n1/n2
+multiplicacion= n1*n2
+
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La división es: ", division)
+print("La multiplicación es: ", multiplicacion)
